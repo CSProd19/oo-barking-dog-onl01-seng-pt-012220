e265541cca35a8e53d66dbec1bdc1c7a59fd3e4c
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-05 03:27:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
-#class Dog 
-#def name=(this_dogs_name)
-#   @this_dogs_name = this_dogs_name
-# end 
-#end 
+class Dog 
+def name=(this_dogs_name)
+   @this_dogs_name = this_dogs_name
+ end 
+end 
 
-#def name 
-#  @this_dogs_name
-#end
-#end 
+def name 
+  @this_dogs_name
+end
+end 
 
 fido = Dog.new 
 fido.name = "Fido"
